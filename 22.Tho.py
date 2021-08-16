c64753f9ec801ef6099a09ef7b8e6e2a62fa3840
God-Chicken <88639770+God-Chicken@users.noreply.github.com>
2021-08-16 11:14:54 +0700

    [add]

---
+++ 22.Tho.py
@@ -0,0 +1,42 @@
+#Data type
+#1.Boolean -- true or false
+#2.Number -- int -- full number or float --decimal
+#3.string "Hello"
+#4.List = [1,2,3,4]
+#5.Dict = {"key" "volue"}
+
+name = "Sasapin Sarawas"
+age = 14
+women = True
+men = False
+money = 1.1
+
+print(type(name), name)
+print(type(age), age)
+print(type(women), women)
+print(type(men), men)
+print(type(money), money)
+
+
+#operator
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#loop
+for i in range(10):
+    i = i+1
+    print(i)
+    if i>=5 and i<9:
+        print(i)
+
+#Ex
+number = 16
+friend = "time"
+friend_gender = "men"
+for x in range(number):
+    if x>7 and x=12:
+        print(x)
