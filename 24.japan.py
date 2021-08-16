426f399a87cb96b77118ed49681e22b169f74b8e
Jaanpan <88639774+Jaanpan@users.noreply.github.com>
2021-08-16 11:11:07 +0700

    ADD

---
+++ 24.japan.py
@@ -0,0 +1,33 @@
+Name = "Suksan Thongsakul"
+age = 15
+Men = True
+Women = False
+Money = 100.50
+score = (2, 4, 6, 8)
+
+print(type(Name), Name)
+print(type(age), age)
+print(type(Men), Men)
+print(type(Women), Women)
+print(type(Money), Money)
+
+
+
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+for i in range(10):
+    i = i+1
+    if i>=5 and i<9:
+        print(i)
+
+number = 24
+friend ="tonkla"
+friend_gender ="male"
+for j in range(24):
+    if j>=7 and j<=12:
+        print(j)
